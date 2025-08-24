e718bfd59408065f55e061daef997c5214dc32e6
Renato <renatojs.br.dev@gmail.com>
2025-08-24 02:09:27 +0000

    adiciona pagina incial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Este código marca o início de uma aplicação simples com o GitHub. Se você curtiu a ideia, contribua com um joinha e ajude a fortalecer o projeto!</h1>
+}
+
+export default Home;
